216890b90dd2a6d2b6cc34814d08e409b5b8b183
Flood99 <113184223+Flood99@users.noreply.github.com>
2024-03-05 12:27:48 -0500

    6u6ryj6jrt6v

--- Adventure.py
+++ Adventure.py
@@ -1,42 +1,54 @@
-    
-import os   
+import os
 os.system("clear")
-
-class Player:
-     def __init__(self,name):
-         self.name = name
-     frontDoorKey = False
-
-
-
 def ScenePitStart():
     inp = input()
     match inp:
         case "look":
-            print("You look")
-            print("The End?")
+            print("You try looking around but its too dark to see anything")
+            ScenePitStart()
             
         case "climb":
-            print("you climb")
-            print("The End?")
+            print("You Grasp at the walls looking for any holds,\nuntil eventually you find one and pull yourself up")
+            input()
+            SceneShedStart()
             
         case _:
             print("Invalid Command")
-            print(input)
             ScenePitStart()
 
-        
+def SceneShedStart():
+
+    os.system("clear")
+    print("You find Yourself in what seems to be a small tool shed.\nTo the North there is a table.\nTo the south there is a door.\nTo the west there is a Window\n\n What will you do")
+    inp = input()
+    match inp:
+        case "North":
+            print("You try looking around but its too dark to see anything")
+            ScenePitStart()
+            
+        case "South":
+            print("You walk over to the door. Should you try to \nOPEN the door\n Go NORTH")
+
+            print("The End?")
+        case "West":
+            print("You walk over to the window\nShould you try to\nLOOK outside,\nOPEN the window\nor go East")
+        case _:
+            print("Invalid Command\n Tip: To move in a direction type the cardinal direction")
+            input()
+            SceneShedStart()
+    
 
     
 def Start():   
     print("Enter a name!")
-    player = Player(input())
-    print("Hello " + player.name + "! Press enter to continue or type back to reenter a name")
+    name = input()
+    print("Hello " + name + "! Press enter to continue or type back to reenter a name")
     if input() == "back":
         Start()
     else:
         print("You wake up in a dark pit\n Tip: Use Enter to move text forward")
         input()
+
         print("You could LOOK around or try to CLIMB out")
         ScenePitStart()
 
